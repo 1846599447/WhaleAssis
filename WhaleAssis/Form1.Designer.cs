74dd90fea18b8c9c92b0486966fb01c7abb9c3c4
another <1846599447@qq.com>
2024-05-14 16:00:13 +0800

    Initial commit

---
+++ WhaleAssis/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace WhaleAssis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.提示词 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(368, 107);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 28);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "1";
+            // 
+            // 提示词
+            // 
+            this.提示词.Location = new System.Drawing.Point(212, 110);
+            this.提示词.Name = "提示词";
+            this.提示词.Size = new System.Drawing.Size(120, 25);
+            this.提示词.TabIndex = 1;
+            this.提示词.Text = "输入月数:";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(212, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "输入邀请码:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(725, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(368, 175);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 28);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(270, 282);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 45);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "确定";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(298, 233);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 31);
+            this.label2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 425);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.提示词);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "WhaleAssis";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Button button2;
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.TextBox textBox2;
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.Label 提示词;
+
+        private System.Windows.Forms.TextBox textBox1;
+
+        #endregion
+    }
+}
---
+++ WhaleAssis/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+
+namespace WhaleAssis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string RandomStr()
+        {
+            Random random = new Random();
+            string characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            char[] result = new char[32];
+
+            for (int i = 0; i < 32; i++)
+            {
+                result[i] = characters[random.Next(characters.Length)];
+            }
+
+            string randomString = new string(result);
+            return randomString;
+        }
+
+        protected string RandomNum()
+        {
+            Random random = new Random();
+            string randomString = "";
+
+            for (int i = 0; i < 12; i++)
+            {
+                randomString += random.Next(10);
+            }
+
+            return randomString;
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            var textBox1Text = textBox1.Text;
+            var inviteCode = textBox2.Text;
+            int number1 = 0;
+            try
+            {
+                number1 = int.Parse(textBox1Text);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("请输入数字！");
+                return;
+            }
+
+
+            for (int i = 0; i < number1; i++)
+            {
+                label2.Text = "正在运行...";
+                try
+                {
+                    using (HttpClient client = new HttpClient())
+                    {
+                        var deviceId = RandomStr();
+                        var email = RandomNum();
+
+                        // 设置请求的URL
+                        string url =
+                            "https://wa01.googla.org/account/register?platform=2&api_version=14&app_version=1.43&lang=zh&_key=&market_id=1000&pkg=com.bjchuhai&device_id=rk_" +
+                            deviceId +
+                            "&sys_version=13&ts=1688957158281&sub_pkg=com.bjchuhai&version_code=43";
+
+                        // 创建要发送的内容
+                        var content = new FormUrlEncodedContent(new Dictionary<string, string>
+                        {
+                            { "passwd", "e99a18c428cb38d5f260853678922e03" },
+                            { "invite_code", inviteCode },
+                            { "email", email + "@gmail.com" }
+                        });
+
+                        // 发送POST请求
+                        HttpResponseMessage response = await client.PostAsync(url, content);
+
+                        // 检查响应是否成功
+                        response.EnsureSuccessStatusCode();
+
+                        // 读取响应内容
+                        string responseContent = await response.Content.ReadAsStringAsync();
+
+                        // 处理响应内容，例如在窗体上显示
+
+                        label2.Text = "第" + (i + 1) + "个月成功";
+                        // 延迟一秒钟
+                        await Task.Delay(1000);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // 处理异常
+                    MessageBox.Show("An error occurred: " + ex.Message);
+                }
+            }
+            button2.Enabled = true;
+            label2.Text = "";
+            MessageBox.Show("全部完成！");
+        }
+    }
+}
